53c828e534fe4ea1cb304f5c278b1001e40953c9
David Urs <david@Davids-MacBook-Air.local>
2023-01-06 13:05:48 +0000

    add ecs

--- main.tf
+++ main.tf
@@ -7,3 +7,87 @@ resource "aws_vpc" "main" {
 }
 
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "public"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.gw.id}"
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_ecs_cluster" "foo" {
+  name = "web-deployments"
+}
+
+
+resource "aws_ecs_task_definition" "nginx" {
+  family = "nginx"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  container_definitions = jsonencode([
+    {
+      name      = "nginx"
+      image     = "nginx"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }])
+}
+
+
+resource "aws_ecs_service" "nginx" {
+  name            = "nginx"
+  cluster         = aws_ecs_cluster.foo.id
+  task_definition = aws_ecs_task_definition.nginx.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  # iam_role        = aws_iam_role.foo.arn
+  # depends_on      = [aws_iam_role_policy.foo]
+
+  network_configuration {
+    subnets = [aws_subnet.main.id]
+    assign_public_ip = true
+    security_groups = ["sg-0179bb03142ac2141"]
+  }
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.foo.arn
+  #   container_name   = "mongo"
+  #   container_port   = 8080
+  # }
+}
